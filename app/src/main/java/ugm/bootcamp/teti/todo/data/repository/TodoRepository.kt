f18c77440338b11ae69d08177348a63061ba3e06
andhikayuana <andhikayuana@gmail.com>
2024-01-11 11:54:55 +0700

    feat: implement scoping todo with authenticated user

--- app/src/main/java/ugm/bootcamp/teti/todo/data/repository/TodoRepository.kt
+++ app/src/main/java/ugm/bootcamp/teti/todo/data/repository/TodoRepository.kt
@@ -12,23 +12,30 @@ interface TodoRepository {
 
     suspend fun all(): List<Todo>
 
-    class Impl : TodoRepository {
+    class Impl(
+        private val authRepository: AuthRepository
+    ) : TodoRepository {
 
-        private val todos: MutableMap<String, Todo> = mutableMapOf()
+        private val todos: MutableMap<String, MutableMap<String, Todo>> = mutableMapOf()
         override suspend fun createOrUpdate(todo: Todo) {
-            todos[todo.id] = todo
+            if (todos.containsKey(authRepository.getAuthenticatedUser().email)) {
+                todos[authRepository.getAuthenticatedUser().email]?.set(todo.id, todo)
+            } else {
+                todos[authRepository.getAuthenticatedUser().email] = mutableMapOf(todo.id to todo)
+            }
         }
 
         override suspend fun get(id: String): Todo? {
-            return todos[id]
+            return todos[authRepository.getAuthenticatedUser().email]?.get(id)
         }
 
         override suspend fun delete(id: String) {
-            todos.remove(id)
+            todos[authRepository.getAuthenticatedUser().email]?.remove(id)
         }
 
         override suspend fun all(): List<Todo> {
-            return todos.values.toList()
+            return todos[authRepository.getAuthenticatedUser().email]?.values?.toList()
+                ?: emptyList()
         }
 
     }
